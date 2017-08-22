2df27d5478b6888ec660f2ffe3ebe4636213bb79
xxcqc250 <a0963272507@gmail.com>
2017-08-22 12:20:36 +0800

    git commit demo

--- KCM_search.py
+++ KCM_search.py
@@ -1,14 +1,44 @@
 import pickle
+import operator
+from collections import OrderedDict
+from operator import itemgetter
 
-with open("wiki_counter_list.txt", "rb") as fp:   # Unpickling
-	counter_list = pickle.load(fp)
-with open("word_dictionary.txt", "rb") as fp:   # Unpickling
-	word_num = pickle.load(fp)
+word_dict ={}
 
-word=input()
-count=input()
 
-print(word_num[word])
+w=open("b.txt","w",encoding="utf-8")
+with open("word_dictionary_DouDic.txt", "rb") as fp:   # Unpickling
+	word_dict = pickle.load(fp)
 
-# w=open("a.txt",'w',encoding="utf8")
-# w.write(str(word_num))
+# word_dict['國文'] ={}
+# word_dict['國文']['數學'] =8
+# word_dict['國文']['英文'] =10
+# word_dict['國文']['化學'] =9
+# word_dict['數學'] ={}
+# word_dict['數學']['國文']=4
+# print(word_dict)
+stop=1
+
+while stop!=0:
+	input_word=input("輸入關鍵字:")
+	input_count=input("次數:")
+
+	# word_dict['國文']=sorted(word_dict['國文'].items(),key=operator.itemgetter(0))
+	# word_dict['國文']=sorted(word_dict['國文'].values())
+	l=OrderedDict(sorted(word_dict[input_word].items(), key=itemgetter(1),reverse=True))
+	# print(word_dict['國文'])
+	count=1
+
+	w=open("result.txt",'w',encoding="utf-8")
+
+	for i in l:
+		count+=1
+		w.write(i+" "+str(l[i])+"\n")
+		if count >int(input_count):
+			# print('enough')
+			break
+	
+# for key,value in word_dict['國文']:
+# 	print(key)
+# input_word=input()
+
